a36ef2ccfe6f39863483905132070c7c380c7529
Allen Marshall <allen.marshall.22@gmail.com>
2019-01-14 20:42:55 -0600

    Changed some code to use constants in the constants module instead of constants scattered throughout the code.

--- src/bundle_model/mod.rs
+++ src/bundle_model/mod.rs
@@ -1,6 +1,5 @@
 //! Defines data structures representing information available in an LV2 bundle's RDF data.
 
 pub mod constants;
-pub mod subclasses;
 pub mod port;
 pub mod plugin;
--- src/bundle_model/port/mod.rs
+++ src/bundle_model/port/mod.rs
@@ -9,28 +9,28 @@ use num_bigint::BigUint;
 use ordered_float::OrderedFloat;
 
 use crate::rdf_util::Literal;
-use crate::bundle_model::subclasses::StdAtomType;
+use crate::bundle_model::constants::{AtomType, Unit, PortProperty, PortDesignation, PortChannel};
 
 /// Contains extra information associated with an LV2 atom port.
 ///
 /// Note: This type's implementations of [`Ord`](std::cmp::Ord) and
 /// [`PartialOrd`](std::cmp::PartialOrd) have little semantic meaning, and exist mainly for use with
 /// collections that require an ordered element type.
-#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
 pub struct AtomPortInfo {
     /// Main atom types that the port can accept in its buffer.
-    main_types: EnumSet<StdAtomType>,
+    main_types: EnumSet<AtomType>,
 
     /// Element types accepted by the element-accepting atom types in
     /// [`main_types`](self::AtomPortInfo::main_types). Must be empty if
     /// [`expects_element_type`](crate::bundle_model::subclasses::StdAtomType::expects_element_type)
     /// is not true for any of the atom types in
     /// [`main_types`](self::AtomPortInfo::main_types).
-    element_types: EnumSet<StdAtomType>
+    element_types: EnumSet<AtomType>
 }
 
 /// Identifiers for port buffer types understood by this crate.
-#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd, Hash)]
 pub enum PortBufferType {
     /// Indicates that the port operates on single-channel audio-rate sample data. Samples are
     /// expected to be of the C `float` type.
@@ -62,42 +62,6 @@ pub struct ScalePoint {
 }
 
 enum_set_type! {
-    /// Enumeration of measurement units defined by the LV2 standard.
-    // TODO: Maybe move this to a different module?
-    pub enum StdUnit {
-        Bar,
-        Beat,
-
-        /// Beats per minute.
-        Bpm,
-
-        Cent,
-        Centimeter,
-        Coefficient,
-        Decibel,
-        Degree,
-        AudioFrame,
-        Hertz,
-        Inch,
-        Kilohertz,
-        Kilometer,
-        Meter,
-        Megahertz,
-        MidiNote,
-        Mile,
-        Minute,
-        Millimeter,
-        Millisecond,
-        Octave,
-        Percent,
-        Second,
-
-        /// Semitone using 12-tone equal temperament.
-        Semitone12Tet
-    }
-}
-
-enum_set_type! {
     /// Enumeration of boolean properties a port can have. Several, but not all, of these flags
     /// correspond to instances of the `lv2:PortProperty` RDF class from the LV2 standard.
     ///
@@ -113,161 +77,9 @@ enum_set_type! {
         /// neither.
         OutputPort,
 
-        /// Indicates that connecting the port to a non-null buffer is optional.
-        ConnOptional,
-
-        /// Indicates that the port's only valid values are those defined by its scale points.
-        Enumeration,
-
-        /// Indicates that the port's only valid values are integers.
-        IntOnly,
-
-        /// Indicates that the port is a `sidechain` rather than a main port. Hosts are not required
-        /// to connect anything to a sidechain port, not even a null pointer.
-        SideChain,
-
-        /// Indicates that the port reports the plugin's latency in samples.
-        ReportsLatency,
-
-        /// Indicates that the port's bounds (e.g. maximum and minimum) should be interpreted as
-        /// multiples of the sample rate.
-        BoundsRelativeToSampleRate,
-
-        /// Indicates that the port's value represents a boolean. Ports with this flag should
-        /// interpret positive values as true, and zero or negative values as false.
-        Toggle,
-
-        /// Indicates that changing the port's input value may cause audio artifacts.
-        ChangeCausesArtifacts,
-
-        /// Indicates that the port's signal should be interpreted as a smooth modulation signal.
-        ContinuousCV,
-
-        /// Indicates that the port's signal should be interpreted as a discrete modulation signal.
-        DiscreteCV,
-
-        /// Indicates that changing the port's input value may trigger expensive computation.
-        ChangeExpensive,
-
-        /// Indicates that the port's bounds (e.g. maximum and minimum) should be considered strict.
-        StrictBounds,
-
-        /// Indicates that the port's value is on a logarithmic scale.
-        Logarithmic,
-
-        /// Indicates that the port is not intended to receive a modulation/automation signal.
-        NotAutomatic,
-
-        /// Indicates that the port is not intended to be shown as a control in the GUI.
-        NotOnGui,
-
-        /// Indicates that the port represents a trigger, and should be reset to its default value
-        /// when not being triggered.
-        Trigger,
-
         /// Indicates that the port may automatically change its port type whenever a port on the
         /// same plugin instance is morphed by the host.
-        AutoMorphable,
-
-        /// Designates the port as a main control channel. Typically used for a MIDI channel that
-        /// controls an instrument plugin.
-        ControlChannel,
-
-        /// Designates the port as a center channel.
-        CenterChannel,
-
-        /// Designates the port as a center left channel.
-        CenterLeftChannel,
-
-        /// Designates the port as a center right channel.
-        CenterRightChannel,
-
-        /// Designates the port as a left channel.
-        LeftChannel,
-
-        /// Designates the port as a low-frequency effects channel.
-        LowFrequencyEffectsChannel,
-
-        /// Designates the port as a rear center channel.
-        RearCenterChannel,
-
-        /// Designates the port as a rear left channel.
-        RearLeftChannel,
-
-        /// Designates the port as a rear right channel.
-        RearRightChannel,
-
-        /// Designates the port as a right channel.
-        RightChannel,
-
-        /// Designates the port as a side channel.
-        SideChannel,
-
-        /// Designates the port as a side left channel.
-        SideLeftChannel,
-
-        /// Designates the port as a side right channel.
-        SideRightChannel,
-
-        /// Designates the port as representing an amplitude.
-        AmplitudeDesignation,
-
-        /// Designates the port as representing an envelope's attack duration.
-        AttackDesignation,
-
-        /// Designates the port as a boolean bypass channel. A value of true means bypassed.
-        BypassDesignation,
-
-        /// Designates the port as representing a cutoff frequency.
-        CutoffFrequencyDesignation,
-
-        /// Designates the port as representing an envelope's decay duration.
-        DecayDesignation,
-
-        /// Designates the port as representing an envelope's delay duration.
-        DelayDesignation,
-
-        /// Designates the port as representing a dry level for a signal.
-        DryLevelDesignation,
-
-        /// Designates the port as representing a frequency.
-        FrequencyDesignation,
-
-        /// Designates the port as representing a gain in decibels.
-        GainDesignation,
-
-        /// Designates the port as representing an envelope's hold duration.
-        HoldDesignation,
-
-        /// Designates the port as representing a rectangular wave's pulse width.
-        PulseWidthDesignation,
-
-        /// Designates the port as representing a compression ratio.
-        CompressionRatioDesignation,
-
-        /// Designates the port as representing an envelope's release duration.
-        ReleaseDesignation,
-
-        /// Designates the port as representing a filter resonance.
-        ResonanceDesignation,
-
-        /// Designates the port as representing a sample rate in Hertz.
-        SampleRateDesignation,
-
-        /// Designates the port as representing an envelope's sustain level.
-        SustainDesignation,
-
-        /// Designates the port as representing a compression threshold.
-        CompressionThresholdDesignation,
-
-        /// Designates the port as representing a waveform.
-        WaveformDesignation,
-
-        /// Designates the port as representing a wet/dry ratio.
-        WetDryRatioDesignation,
-
-        /// Designates the port as representing a wet level for a signal.
-        WetLevelDesignation
+        AutoMorphable
     }
 }
 
@@ -278,8 +90,17 @@ enum_set_type! {
 /// collections that require an ordered element type.
 #[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd, Hash)]
 pub struct Port {
-    /// Boolean properties.
+    /// Some boolean properties.
     flags: EnumSet<PortFlags>,
+
+    /// Standard LV2 port properties that apply to the port.
+    port_props: EnumSet<PortProperty>,
+
+    /// Standard LV2 designations that apply to the port.
+    designations: EnumSet<PortDesignation>,
+
+    /// Standard LV2 channel designations that apply to the port.
+    channel_designations: EnumSet<PortChannel>,
 
     /// Index of the port.
     index: Option<u32>,
@@ -309,7 +130,7 @@ pub struct Port {
     default_value: Option<Literal>,
 
     /// Identifies the units for the port value.
-    unit: Option<StdUnit>,
+    unit: Option<Unit>,
 
     /// Set of the port's scale points, i.e. marked values that are special in some way.
     scale_points: BTreeSet<ScalePoint>,
@@ -318,10 +139,10 @@ pub struct Port {
     /// the port value through a stepwise interface, such as arrow keys on the keyboard.
     num_range_steps: Option<BigUint>,
 
-    /// Buffer types to which this port can be morphed by the host. An empty set means the host
-    /// cannot change the buffer type.
+    /// Standard buffer types to which this port can be morphed by the host. An empty set means the
+    /// host cannot change the buffer type.
     host_morph_types: BTreeSet<PortBufferType>,
 
-    /// Buffer types supported by this port, before any morphing occurs.
+    /// Standard buffer types supported by this port, before any morphing occurs.
     buffer_types: BTreeSet<PortBufferType>
 }
