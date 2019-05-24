bd051b0431e8cf3b93f7ae54ca104ea1527e4f36
Allen Marshall <allen.marshall.22@gmail.com>
2019-05-24 17:58:33 -0500

    Added representation of required and supported host features in DynManifestInfo.

--- src/bundle_model/dyn_manifest/mod.rs
+++ src/bundle_model/dyn_manifest/mod.rs
@@ -1,8 +1,10 @@
 //! Representation of LV2 dynamic manifest generator.
 
 use crate::rdf_util::Iri;
-use crate::bundle_model::LoadableEntity;
+use crate::bundle_model::{LoadableEntity, HostFeatureSupporter};
 use crate::bundle_model::symbol::Symbol;
+use enumset::{EnumSet, EnumSetIter};
+use crate::bundle_model::constants::HostFeature;
 
 /// Representation of an LV2 dynamic manifest generator.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -14,7 +16,15 @@ pub struct DynManifestInfo {
     binary: Iri,
 
     /// LV2 symbol identifying the dynamic manifest generator.
-    symbol: Option<Symbol>
+    symbol: Option<Symbol>,
+
+    /// Host features required by the dynamic manifest generator. This set should not intersect with
+    /// [`optional_host_features`](self::DynManifestInfo::optional_host_features).
+    required_host_features: EnumSet<HostFeature>,
+
+    /// Host features optionally supported by the dynamic manifest generator. This set should not
+    /// intersect with [`required_host_features`](self::DynManifestInfo::required_host_features).
+    optional_host_features: EnumSet<HostFeature>
 }
 
 impl LoadableEntity for DynManifestInfo {
@@ -28,5 +38,19 @@ impl LoadableEntity for DynManifestInfo {
 
     fn symbol(&self) -> Option<&Symbol> {
         self.symbol.as_ref()
+    }
+}
+
+impl HostFeatureSupporter for DynManifestInfo {
+    type RequiredHostFeaturesIter = EnumSetIter<HostFeature>;
+
+    type OptionalHostFeaturesIter = EnumSetIter<HostFeature>;
+
+    fn required_host_features_iter(&self) -> Self::RequiredHostFeaturesIter {
+        self.required_host_features.iter()
+    }
+
+    fn optional_host_features_iter(&self) -> Self::OptionalHostFeaturesIter {
+        self.optional_host_features.iter()
     }
 }
