e158a14f27c70fbfd6c0ecc7add786e13e59e715
Allen Marshall <allen.marshall.22@gmail.com>
2019-06-01 17:59:39 -0500

    Simplified some traits.

--- src/bundle_model/mod.rs
+++ src/bundle_model/mod.rs
@@ -80,45 +80,6 @@ pub trait OptionallyIdentifiedBy<T> {
     fn id(&self) -> Option<&T>;
 }
 
-/// Trait for types that can contain LV2 name information, including multilingual names.
-pub trait Named<'a> {
-    /// Type of (parallel) iterator returned by [`names_iter`](self::Named::names_iter).
-    type NamesIter: ParallelIterator<Item = &'a Literal>;
-
-    /// Type of (parallel) iterator returned by [`short_names_iter`](self::Named::short_names_iter).
-    type ShortNamesIter: ParallelIterator<Item = &'a Literal>;
-
-    /// Gets a (parallel) iterator over the human-readable name literals for the entity. An entity
-    /// may have multiple language-tagged name literals to provide multilingual naming. It also may
-    /// have no name literals at all, in which case the returned iterator must be empty.
-    fn names_iter(&'a self) -> Self::NamesIter;
-
-    /// Gets a (parallel) iterator over the short name literals for the entity. An entity may have
-    /// multiple language-tagged short name literals to provide multilingual naming. It also may
-    /// have no short name literals at all, in which case the returned iterator must be empty. Note
-    /// that the set of languages supported by the short name literals doesn't have to correspond to
-    /// the set of languages supported by the long name literals.
-    ///
-    /// Implementors must ensure that each returned literal's contents (excluding any language tag
-    /// or data type information) does not exceed 16 Unicode grapheme clusters.
-    fn short_names_iter(&'a self) -> Self::ShortNamesIter;
-}
-
-/// Trait for types that can contain LV2 documentation information, including multilingual
-/// documentation.
-pub trait Documented<'a> {
-    /// Type of (parallel) iterator returned by
-    /// [`documentation_iter`](self::Documented::documentation_iter).
-    type DocumentationIter: ParallelIterator<Item = &'a Literal>;
-
-    /// Gets a (parallel) iterator over the documentation literals for the entity. An entity may
-    /// have multiple language-tagged documentation literals to provide multilingual documentation.
-    /// It also may have no documentation literals at all, in which case the returned iterator must
-    /// be empty. The LV2 specification specifies that the contents of each literal should be "a
-    /// valid XHTML Basic 1.1 fragment suitable for use as the content of the \<body\> element."
-    fn documentation_iter(&'a self) -> Self::DocumentationIter;
-}
-
 /// Defines basic behavior for loadable LV2 entities described in an LV2 bundle, such as plugins,
 /// UIs, and dynamic manifest generators.
 pub trait Loadable {
@@ -127,109 +88,65 @@ pub trait Loadable {
     fn binary(&self) -> Option<&Iri>;
 }
 
-/// Trait for types that can specify a set of "provided" elements.
+/// Trait for types that are in some way related to a set of values. The type of relationship is
+/// defined by the `R` parameter.
 ///
 /// # Parameters
-/// - `T`: Type of provided element.
-pub trait Provider<'a, T: Sync> {
-    /// Type of borrowed element returned by the iterators produced by this trait's methods.
+/// - `R`: Defines the way in which the set elements relate to the instance that "has" the set.
+/// - `T`: Type for elements in the set.
+pub trait HasRelatedSet<R, T: Sync> {
+    /// Type of borrowed element returned by the set iterator.
     type BorrowedElt: Borrow<T> + Send;
 
-    /// Type of (parallel) iterator returned by [`provided_iter`](self::Provider::provided_iter).
-    type ProvidedIter: ParallelIterator<Item = Self::BorrowedElt>;
+    /// Type of (parallel) iterator returned by [`set_iter`](self::HasRelatedSet::set_iter).
+    type SetIter: ParallelIterator<Item = Self::BorrowedElt>;
 
-    /// Gets a (parallel) iterator over the provided elements. The iterator must not repeat items.
-    fn provided_iter(&'a self) -> Self::ProvidedIter;
+    /// Gets a (parallel) iterator over the elements of the set. The returned iterator must not
+    /// repeat elements, and may be empty.
+    fn set_iter(&self) -> Self::SetIter;
 
-    /// Checks if the specified element is provided. Must return true if and only if `to_check` is
-    /// in the set that would be returned by [`provided_iter`](self::Provider::provided_iter). The
-    /// default implementation calls [`provided_iter`](self::Provider::provided_iter) and searches
-    /// for `to_check`; a more efficient implementation is likely possible for most implementing
-    /// types.
-    ///
-    /// # Parameters
-    /// - `to_check`: The providable element to check for.
-    fn provides(&'a self, to_check: &T) -> bool
+    /// Checks if the specified element is in the set. The default implementation calls
+    /// [`set_iter`](self::HasRelatedSet::set_iter) and searches for the specified element. A more
+    /// efficient implementation is likely possible for most implementing types.
+    fn has_elt(&self, to_check: &T) -> bool
         where T: Eq
     {
-        self.provided_iter().any(|elt| elt.borrow() == to_check)
+        self.set_iter().any(|elt| elt.borrow() == to_check)
     }
 }
 
-/// Trait for types that can specify sets of "required" and "optionally supported" elements.
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are names for the instance that "has" the set. Multiple language-tagged names are allowed in
+/// order to support multilingual naming.
+pub enum NameRelation {}
+
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are short names for the instance that "has" the set. This is similar to
+/// [`NameRelation`](self::NameRelation), except that the name strings are expected to be no more
+/// than 16 characters.
+pub enum ShortNameRelation {}
+
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are documentation strings for the instance that "has" the set. Multiple language-tagged
+/// documentation strings are allowed in order to support multilingual documentation.
 ///
-/// # Parameters
-/// - `T`: Type of supported element.
-pub trait Requirer<'a, T: Sync> {
-    /// Type of borrowed element returned by the iterators produced by this trait's methods.
-    type BorrowedElt: Borrow<T> + Send;
+/// The LV2 specification specifies that each documentation string's contents should be "a valid
+/// XHTML Basic 1.1 fragment suitable for use as the content of the \<body\> element."
+pub enum DocRelation {}
 
-    /// Type of (parallel) iterator returned by
-    /// [`required_iter`](self::Requirer::required_iter).
-    type RequiredIter: ParallelIterator<Item = Self::BorrowedElt>;
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are type/class tags for the instance that "has" the set.
+pub enum TypeRelation {}
 
-    /// Type of (parallel) iterator returned by
-    /// [`optionally_supported_iter`](self::Requirer::optionally_supported_iter).
-    type OptionallySupportedIter: ParallelIterator<Item = Self::BorrowedElt>;
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are identifiers for items that are in some way provided by the instance that "has" the set.
+pub enum ProvidesRelation {}
 
-    /// Gets a (parallel) iterator over the required elements. The iterator must not repeat items,
-    /// and its contents must be disjoint from those returned by
-    /// [`optionally_supported_iter`](self::Requirer::optionally_supported_iter).
-    fn required_iter(&'a self) -> Self::RequiredIter;
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are identifiers for items that are in some way required by the instance that "has" the set.
+pub enum RequiresRelation {}
 
-    /// Checks if the specified element is required. Must return true if and only if
-    /// `to_check` is in the set that would be returned by
-    /// [`required_iter`](self::Requirer::required_iter).
-    /// The default implementation calls [`required_iter`](self::Requirer::required_iter) and
-    /// searches for `to_check`; a more efficient implementation is likely to be possible for most
-    /// implementing types.
-    ///
-    /// # Parameters
-    /// - `to_check`: The element to check for.
-    fn requires(&'a self, to_check: &T) -> bool
-        where T: Eq
-    {
-        self.required_iter().any(|elt| elt.borrow() == to_check)
-    }
-
-    /// Gets a (parallel) iterator over the optionally supported elements. The iterator must not
-    /// repeat items, and its contents must be disjoint from those returned by
-    /// [`required_iter`](self::Requirer::required_iter).
-    fn optionally_supported_iter(&'a self) -> Self::OptionallySupportedIter;
-
-    /// Checks if the specified element is optionally required. Must return true if and only if
-    /// `to_check` is in the set that would be returned by
-    /// [`optionally_supported_iter`](self::Requirer::optionally_supported_iter).
-    /// The default implementation calls
-    /// [`optionally_supported_iter`](self::Requirer::optionally_supported_iter) and searches for
-    /// `to_check`; a more efficient implementation is likely to be possible for most implementing
-    /// types.
-    ///
-    /// # Parameters
-    /// - `to_check`: The element to check for.
-    fn optionally_supports(&'a self, to_check: &T) -> bool
-        where T: Eq
-    {
-        self.optionally_supported_iter().any(|elt| elt.borrow() == to_check)
-    }
-
-    /// Gets a (parallel) iterator over the elements that are supported, as either required elements
-    /// or optional elements. The iterator must not repeat items, and must contain a set equal to
-    /// the union of the sets returned by [`required_iter`](self::Requirer::required_iter) and
-    /// [`optionally_supported_iter`](self::Requirer::optionally_supported_iter).
-    fn supported_iter(&'a self) -> Chain<Self::RequiredIter, Self::OptionallySupportedIter> {
-        self.required_iter().chain(self.optionally_supported_iter())
-    }
-
-    /// Checks if the specified element is supported, as either a required element or an optional
-    /// element. Must return true if and only if `to_check` is in the set that would be returned by
-    /// [`supported_iter`](self::Requirer::supported_iter).
-    ///
-    /// # Parameters
-    /// - `to_check`: The element to check for.
-    fn supports(&'a self, to_check: &T) -> bool
-        where T: Eq
-    {
-        self.requires(to_check) || self.optionally_supports(to_check)
-    }
-}
+/// Relation type for [`HasRelatedSet`](self::HasRelatedSet), which indicates that the set elements
+/// are identifiers for items that are in some way optionally supported by the instance that "has"
+/// the set.
+pub enum OptionallySupportsRelation {}
